8ce67470244ba026690b7a26db3c57b26dfc1bd8
MatthewJ121 <matthew.jhs.2005@gmail.com>
2025-01-13 23:11:01 -0500

    broken learning code

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+guildID = 1328458609163763804
+
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
@@ -11,13 +13,19 @@ async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("Bot is ready!")
 
-@bot.slash_command(guild_ids=[1328458609163763804], name="ping", description="check latency")
+@bot.slash_command(guild_ids=guildID, name="ping", description="check latency")
 async def ping(ctx):
     """
     replies with ping
     """
     await ctx.respond(f"your ping is {round(bot.latency * 1000)}ms")
 
+@bot.slash_command(guild_ids=guildID, name="lookup", description="see a user or somthin")
+async def lookup(ctx, *, user:str):
+    """
+    takes str input from user in username field and searches for a roblox acct
+    """
+    await ctx.send(user)
+
 
 bot.run(os.getenv('token'))
-#test
