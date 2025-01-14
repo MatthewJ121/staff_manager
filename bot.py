7c44eefb28f9d37675e3aa989f265761522bf29c
gorillapain <125838234+gorillapain4626@users.noreply.github.com>
2025-01-13 23:09:17 -0500

    added some more commands

--- bot.py
+++ bot.py
@@ -6,11 +6,25 @@ from dotenv import load_dotenv
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
+######################################################
+
+guildID = 1328458609163763804
+
+######################################################
+
+# prints when the bot is ready
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("Bot is ready!")
 
+# attempts to replicate viewprofile command from the other bot
+@bot.slash_command(guild_ids=[1328458609163763804], name="viewprofile", description="view the profile of a user")
+async def profile(ctx, *, username: str):
+
+    await ctx.respond(f"Username: {username}")
+   
+# checks latency
 @bot.slash_command(guild_ids=[1328458609163763804], name="ping", description="check latency")
 async def ping(ctx):
     """
@@ -18,6 +32,16 @@ async def ping(ctx):
     """
     await ctx.respond(f"your ping is {round(bot.latency * 1000)}ms")
 
+# sends an embed message
+@bot.slash_command(guild_ids=[1328458609163763804], name="embed", description="test embed capabilities")
+async def embed_test(ctx):
+    """
+   tests embeds
+    """
+    embed = discord.Embed(title="embed test", description="this is a test embed message", color=discord.Color.green())
+    embed.add_field(name="field 1", value="value 1", inline=False)
+    embed.set_footer(text="footer text")
+    await ctx.respond(embed=embed)
 
 bot.run(os.getenv('token'))
 #test
