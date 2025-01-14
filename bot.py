3809eb6ccf6120a2f6d05127d58f0616fa890142
MatthewJ121 <matthew.jhs.2005@gmail.com>
2025-01-14 15:39:58 -0500

    broken additions to viewprofile

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ from discord.ext import commands
 import os
 import requests
 from dotenv import load_dotenv
+import datetime
+
+date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+current_date = datetime.datetime.now()
+print(f"date is {current_date}")
 
 guildID = [1328458609163763804]
 
@@ -21,13 +26,23 @@ async def on_ready():
 
 
 ## attempts to replicate viewprofile command from the other bot
-@bot.slash_command(guild_ids=guildID, name="viewprofile", description="view the profile of a user")
-async def profile(ctx, username: str):
+@bot.slash_command(guild_ids=guildID, description="view the profile of a user")
+async def viewprofile(ctx, username: str):
 
     userid_request = requests.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [username], "excludeBannedUsers": True}).json()["data"][0]["id"]
     profile_ref = requests.get(f"https://apis.roblox.com/cloud/v2/users/{userid_request}", headers={"x-api-key":os.getenv("roblox_api")}).json()
 
-    embed = discord.Embed(title=profile_ref["name"], description="display name:"+profile_ref["displayName"]+" \n locale:"+profile_ref["locale"]+" \n createTime"+profile_ref["createTime"])
+    creation_date = datetime.datetime.strptime(profile_ref["createTime"], date_format)
+    print(f"AGHHJHHHHHH{creation_date}")
+    creation_date = str(creation_date[0:creation_date.index(" ")])
+    print(creation_date)
+
+    embed = discord.Embed(
+        title=profile_ref["name"], 
+        description=
+        "Display name: " + profile_ref["displayName"] +
+        "\n ID :" + profile_ref["id"] + 
+        "\n Age: " + (current_date - creation_date))
     await ctx.respond(embed=embed)
 
 ## checks latency
@@ -38,7 +53,6 @@ async def ping(ctx):
     """
     await ctx.respond(f"your ping is {round(bot.latency * 1000)}ms")
 
-
 ## sends an embed message
 @bot.slash_command(guild_ids=guildID, description="test embed capabilities")
 async def embed_test(ctx):
@@ -46,7 +60,6 @@ async def embed_test(ctx):
     embed.add_field(name="field 1", value="value 1", inline=False)
     embed.set_footer(text="footer text")
     await ctx.respond(embed=embed)
-
 
 ## runs the bot
 bot.run(os.getenv('token'))
