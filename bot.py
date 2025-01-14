0174907b4dafdea5a018c8026b76d4300f0168ea
gorillapain <125838234+gorillapain4626@users.noreply.github.com>
2025-01-13 23:09:32 -0500

    idk its telling me to commit this too

--- bot.py
+++ bot.py
@@ -12,19 +12,21 @@ guildID = 1328458609163763804
 
 ######################################################
 
-# prints when the bot is ready
+## prints when the bot is ready
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("Bot is ready!")
 
-# attempts to replicate viewprofile command from the other bot
+
+## attempts to replicate viewprofile command from the other bot
 @bot.slash_command(guild_ids=[1328458609163763804], name="viewprofile", description="view the profile of a user")
 async def profile(ctx, *, username: str):
 
     await ctx.respond(f"Username: {username}")
    
-# checks latency
+
+## checks latency
 @bot.slash_command(guild_ids=[1328458609163763804], name="ping", description="check latency")
 async def ping(ctx):
     """
@@ -32,7 +34,8 @@ async def ping(ctx):
     """
     await ctx.respond(f"your ping is {round(bot.latency * 1000)}ms")
 
-# sends an embed message
+
+## sends an embed message
 @bot.slash_command(guild_ids=[1328458609163763804], name="embed", description="test embed capabilities")
 async def embed_test(ctx):
     """
@@ -43,5 +46,7 @@ async def embed_test(ctx):
     embed.set_footer(text="footer text")
     await ctx.respond(embed=embed)
 
+
+## runs the bot
 bot.run(os.getenv('token'))
 #test
