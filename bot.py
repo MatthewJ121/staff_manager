3388cc0c273661dffbfe879fb77b075491cfb3c7
gorillapain <125838234+gorillapain4626@users.noreply.github.com>
2025-01-14 14:04:34 -0500

    - added mongodb basics - fixed error with viewprofile command (forgot to convert to string) - centralized env variables - improved commenting

--- bot.py
+++ bot.py
@@ -4,39 +4,54 @@ import os
 import httpx
 from dotenv import load_dotenv
 
+guildID = [1328458609163763804] # guild id variable so commands update faster
+
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
-######################################################
+token = os.getenv('TOKEN')
+roblox_api = os.getenv('roblox_api')
 
-guildID = [1328458609163763804]
+### MongoDB Handling ###
 
-######################################################
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-## prints when the bot is ready
+uri = (os.getenv('URI'))
+mongoclient = MongoClient(uri)
+rdb = mongoclient.SilverOaks.ResidentList
+bdb = mongoclient.SilverOaks.Blacklist
+sdb = mongoclient.SilverOaks.StaffTracker
+
+blgroups = [9688364, 10085029, 33263569, 34603205, 34549414, 34941244, 9927554]
+
+### ###
+
+
+## prints to console when the bot is ready
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("Bot is ready!")
+    print("the bot has awoken")
 
 
 ## attempts to replicate viewprofile command from the other bot
-@bot.slash_command(guild_ids=guildID, name="viewprofile", description="view the profile of a user")
+@bot.slash_command(guild_ids=guildID, name="viewprofile", description="View the profile of a user.")
 async def profile(ctx, username: str):
 
     # get player's uid from their name, handle if no name found
     userid_request = httpx.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [username], "excludeBannedUsers": True}).json()["data"]
 
     if len(userid_request) == 0:
-        error_embed = discord.Embed(title="No User Found",description=f"There is no user with the name **{username}**.\nCheck your spelling and try again.")
+        error_embed = discord.Embed(title="No User Found",description=f"There is no user with the name **{username}**.\nCheck your spelling and try again.",color=discord.Color.red())
         await ctx.respond(embed=error_embed)
         return
 
     # get player's profile from uid                      
-    profile_ref = httpx.get("https://apis.roblox.com/cloud/v2/users/"+userid_request[0]["id"], headers={"x-api-key":os.getenv("roblox_api")}).json()
+    profile_ref = httpx.get("https://apis.roblox.com/cloud/v2/users/"+str(userid_request[0]["id"]), headers={"x-api-key":(roblox_api)}).json()
 
     embed = discord.Embed(
         title=profile_ref["name"],
-        description="display name:"+profile_ref["displayName"]+" \n locale:"+profile_ref["locale"]+" \n createTime"+profile_ref["createTime"])
+        description="Display Name: "+profile_ref["displayName"]+" \n Locale: "+profile_ref["locale"]+" \n Date Created: "+profile_ref["createTime"], color=discord.Color.green())
     await ctx.respond(embed=embed)
 
 ## sends an embed message
@@ -52,5 +67,5 @@ async def embed_test(ctx):
 
 
 ## runs the bot
-bot.run(os.getenv('token'))
+bot.run(token)
 #test
